3d56d4af200795cb9bdb9652be5c55e21528afc7
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-01 16:31:03 -0700

    Add Lesson 1 Assignment: OO Rock Paper Scissors
    Initial RPSGame object and createPlayer() skeleton

---
+++ lesson_1/rock_paper_scissors.js
@@ -0,0 +1,40 @@
+/**
+ * JS120 Lesson 1
+ * Assignment: Rock Paper Scissors
+ *
+ * An Object-Oriented Rock Paper Scissors game
+ */
+
+function createPlayer() {
+  return {
+    // possible state: player name?
+    // possible state: player's current move?
+
+    choose() {
+      // not yet implemented
+    },
+  };
+}
+
+const RPSGame = {
+  human: createPlayer(),
+  computer: createPlayer(),
+
+  displayWelcomeMessage() {
+    console.log("Welcome to Rock, Paper, Scissors!");
+  },
+
+  displayGoodbyeMessage() {
+    console.log("Thanks for playing Rock, Paper, Scissors. Goodbye!");
+  },
+
+  play() {
+    this.displayWelcomeMessage();
+    this.human.choose();
+    this.computer.choose();
+    displayWinner();
+    this.displayGoodbyeMessage();
+  },
+};
+
+RPSGame.play();
