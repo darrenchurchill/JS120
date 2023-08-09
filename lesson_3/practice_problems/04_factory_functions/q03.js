67d26a053e183f8d5b6c5ca4168ae78f1489b4c0
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-09 14:37:11 -0700

    Add Lesson 3 Assignment 4: Factory Functions Q3

---
+++ lesson_3/practice_problems/04_factory_functions/q03.js
@@ -0,0 +1,40 @@
+/**
+ * JS120 Lesson 3
+ * Practice Problems: Assignment 4: Factory Functions
+ * Question 3
+ *
+ * To process multiple invoices, we need a factory method that we can use to
+ * create invoices. The requirements for the factory function are as follows:
+ *
+ * 1. It returns an invoice object, with `phone` and `internet` properties, and
+ *    a `total` method.
+ * 2. The default value for the phone service is 3000, and the internet service
+ *    is 5500 (in cents, of course1).
+ * 3. The function takes an argument whose attributes override the default
+ *    values.
+ */
+
+function createInvoice(services) {
+  // implement the factory function here
+}
+
+function invoiceTotal(invoices) {
+  let total = 0;
+
+  for (let index = 0; index < invoices.length; index += 1) {
+    total += invoices[index].total();
+  }
+
+  return total;
+}
+
+let invoices = [];
+invoices.push(createInvoice());
+invoices.push(createInvoice({ internet: 6500 }));
+invoices.push(createInvoice({ phone: 2000 }));
+invoices.push(createInvoice({
+  phone: 1000,
+  internet: 4500,
+}));
+
+console.log(invoiceTotal(invoices)); // 31000
