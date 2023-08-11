e64c3f13f48aec2773fb60bd22056c778924044f
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-10 20:18:22 -0700

    Add Lesson 3 Assignment 9: Constructors and Prototypes Q7

---
+++ lesson_3/practice_problems/09_constructors_prototypes/q07.js
@@ -0,0 +1,23 @@
+/**
+ * JS120 Lesson 3
+ * Practice Problems: Assignment 9: Constructors and Prototypes
+ * Question 7
+ *
+ * In this problem, we'll ask you to create a new instance of an object, without
+ * having direct access to the constructor function:
+ */
+
+let ninjaA;
+
+{
+  const Ninja = function() {
+    this.swung = false;
+  };
+
+  ninjaA = new Ninja();
+}
+
+// create a `ninjaB` object here; don't change anything else
+let ninjaB = new ninjaA.constructor();
+
+console.log(ninjaA.constructor === ninjaB.constructor); // => true
